65ae61e3865fdf697ecc0ac6bf636527cdfb5376
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2022-09-05 17:20:38 -0600

    feature: added authorization in areas

--- src/routes/area.routes.ts
+++ src/routes/area.routes.ts
@@ -22,7 +22,7 @@ const router = Router();
 
 // Areas routes
 router.get('/api/areas', findAreasHandler);
-router.get('/api/areas/:areaId', validateResource(getAreaSchema), findAreaHandler);
+router.get('/api/areas/:areaId', [requireUser, validateResource(getAreaSchema)], findAreaHandler);
 router.post('/api/areas', [requireUser, validateResource(createAreaSchema)], createAreaHandler);
 router.put('/api/areas/:areaId', [requireUser, validateResource(updateAreaSchema)], updateAreaHandler);
 router.delete('/api/areas/:areaId', [requireUser, validateResource(deleteAreaSchema)], deleteAreaHandler);
