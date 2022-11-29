f490ecf75afc155b37ae370bdc47a9a01b127307
Marvin Toro <ingricardotoro@gmail.com>
2022-11-28 23:34:34 -0600

    add cors security

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -6,8 +6,15 @@ import deserializeUser from '../middlewares/deserializeUser';
 function createServer() {
   const app = express();
 
+  const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+
+  app.use(cors(corsOptions));
   app.options('*', cors());
-  app.use(cors());
+  //app.use(cors());
   app.use(express.json());
   app.use(deserializeUser);
 
