bef1d0f709d5bdf714c0d72bd1d49da8b3525611
Marvin Toro <ingricardotoro@gmail.com>
2022-11-29 08:29:27 -0600

    add domai in cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -7,13 +7,13 @@ function createServer() {
   const app = express();
 
   const corsOptions = {
-    origin: '*',
+    origin: 'https://dataplushn.com',
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   };
 
   app.use(cors(corsOptions));
-  //app.options('*', cors());
+  app.options('https://dataplushn.com', cors());
   //app.use(cors());
   app.use(express.json());
   app.use(deserializeUser);
