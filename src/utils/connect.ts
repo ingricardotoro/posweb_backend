e62c3b4dcc5a3efccb08bc967d614fc84ca03d22
Marvin Toro <ingricardotoro@gmail.com>
2022-11-30 22:52:45 -0600

    add conection string

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,15 +1,17 @@
-import mongoose from "mongoose";
-import config from "config";
-import logger from "./logger";
+import mongoose from 'mongoose';
+import config from 'config';
+import logger from './logger';
 
 async function connect() {
-  const dbUri = config.get<string>("dbUri");
-  
+  //const dbUri = config.get<string>('dbUri');
+
   try {
-    await mongoose.connect(dbUri);
+    await mongoose.connect(
+      'mongodb+srv://emsanchez:S6ujuxdmezIzMfCZ@posweb.kgexog5.mongodb.net/posweb?retryWrites=true&w=majority'
+    );
     logger.info(`DB connected ${process.env.NODE_ENV}`);
   } catch (error) {
-    logger.error("Could not connect to db");
+    logger.error('Could not connect to db');
     process.exit(1);
   }
 }
