be88c97f8daa7252e987fae4801e23e3b296c39f
Marvin Toro <ingricardotoro@gmail.com>
2022-12-03 11:47:25 -0600

    test cors

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import router from '../routes';
 import deserializeUser from '../middlewares/deserializeUser';
+import cookieParser from 'cookie-parser';
 
 function createServer() {
   const app = express();
@@ -14,24 +15,27 @@ function createServer() {
   };
 
   app.use(cors(corsOptions));
+
   app.use(cors());
   app.options('*', cors());
-  app.use(
+  /* app.use(
     cors({
       origin: ['https://dataplushn.com/', 'http://localhost:5173'],
       //origin: 'http://localhost:5173',
     })
-  );
+  ); */
 
-  app.use((req, res, next) => {
+  /*   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Methods', 'Authorization');
     next();
-  });
+  }); */
 
   app.use(express.json());
   app.use(deserializeUser);
+
+  app.use(cookieParser());
 
   app.use(router);
 
